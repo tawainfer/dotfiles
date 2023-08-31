4bbf1b4b1b1117a4e1fc73419d98fb9dc5f8125e
tawainfer <tawainfer@gmail.com>
2023-08-31 14:10:41 +0900

    リンクを張るスクリプトを修正

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-for path in $(cat link_list.txt)
+script_path=$(readlink -f "$0")
+if [[ $script_path != "${HOME}/dotfiles/scripts/link.sh" ]]
+then
+  echo 'Clone the directory to the correct path.'
+  exit
+fi
+
+dotfiles_path=$(dirname $(dirname $script_path))
+cd $dotfiles_path
+
+for path in $(cat ${dotfiles_path}/link_list.txt)
 do
-  bef="../$path"
+  bef="$dotfiles_path/$path"
   aft="${HOME}/$path"
 
   mkdir $(dirname $aft)
-  ln -fsn ../$path ${HOME}/$path
+  ln -fsn $bef $aft
 done
