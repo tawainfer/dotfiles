6482808e6a1290b64e4e66c4d69f62ba4e175ab6
tawainfer <tawainfer@gmail.com>
2023-08-31 20:47:17 +0900

    リンクを張るスクリプトを修正

--- scripts/link.sh
+++ scripts/link.sh
@@ -12,9 +12,10 @@ cd $dotfiles_path
 
 for path in $(cat ${dotfiles_path}/link_list.txt)
 do
-  bef="$dotfiles_path/$path"
+  bef="${dotfiles_path}/$path"
   aft="${HOME}/$path"
 
+  rm -rfv $aft
   mkdir $(dirname $aft)
-  ln -fsnv $bef $aft
+  ln -fsnv "$bef" "$aft"
 done
