101c1ea41ed1bb8eb2076cb86a17ddd509d38cc9
tawainfer <tawainfer@gmail.com>
2023-09-14 14:03:30 +0900

    リンクを張るスクリプトを修正

--- scripts/link.sh
+++ scripts/link.sh
@@ -3,7 +3,7 @@
 script_path=$(readlink -f "$0")
 dotfiles_path=$(dirname $(dirname $script_path))
 
-for path in $(cat link/link_list)
+for path in $(cat list/link_list)
 do
   bef="${dotfiles_path}/$path"
   aft="${HOME}/$path"
