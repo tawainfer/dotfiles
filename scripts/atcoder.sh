1cc30e68735e6f6f08d25c5209efb4008dc65473
tawainfer <tawainfer@gmail.com>
2023-09-14 09:33:58 +0900

    atcoderの環境を作成するスクリプトを修正

--- scripts/atcoder.sh
+++ scripts/atcoder.sh
@@ -8,14 +8,11 @@ mkdir work
 cd work
 touch 'a.cpp' 'a.py' 'Main.java'
 
-cd $dir
-zip='v1.4.zip'
-if [ -e $zip ]
-then
-  echo 'The zip file already exists.'
-else
-  wget "https://github.com/atcoder/ac-library/archive/refs/tags/${zip}"
-fi
 
-unzip -o $zip
-rm -rf $zip
+cd $dir
+if [ -d 'ac-library' ]
+then
+  echo 'ac-library already exists.'
+else
+  git clone https://github.com/atcoder/ac-library.git
+fi
