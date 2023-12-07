b06cb28dbaa1c67ab214a9be04389c3b095aab06
tawainfer <tawainfer@gmail.com>
2023-12-07 15:26:59 +0900

    プロンプトでホスト名を表示するデフォルトのカラーを変更

--- scripts/host_rgb.sh
+++ scripts/host_rgb.sh
@@ -5,6 +5,6 @@ cd $(dirname ${BASH_SOURCE[0]})
 path="${HOME}/.host_rgb"
 if [ ! -e $path ]
 then
-  echo '0 200 100' > $path
+  echo '80 250 123' > $path
   echo 'create ".host_rgb" at home directory'
 fi
